e7d4708570e020018a5b3c52f9c5a9a7337fe045
Schaller, Dennis <dennis.schaller@accenture.com>
2019-09-18 22:41:58 +0200

    chore(fuse): Adding asset copy - WIP.

--- fuse.ts
+++ fuse.ts
@@ -1,35 +1,38 @@
-import { FuseBox, CSSPlugin, Sparky, CopyPlugin, ReplacePlugin } from "fuse-box"
-import { spawn } from "child_process"
-import * as pjson from "./package.json"
+import { FuseBox, CSSPlugin, Sparky, CopyPlugin, ReplacePlugin } from "fuse-box";
+import { spawn } from "child_process";
+import * as pjson from "./package.json";
+import * as fs from "fs";
+import * as path from "path";
 
-const DEV_PORT = 4445
-const OUTPUT_DIR = "out"
-const ASSETS = ["*.jpg", "*.png", "*.jpeg", "*.gif", "*.svg"]
+const DEV_PORT = 4445;
+const OUTPUT_DIR = "out";
+const SRC_DIR = "src";
+const ASSETS = ["*.jpg", "*.png", "*.jpeg", "*.gif", "*.svg"];
 
 // are we running in production mode?
-const isProduction = process.env.NODE_ENV === "production"
+const isProduction = process.env.NODE_ENV === "production";
 
 // copy the renderer's html file into the right place
 Sparky.task("copy-html", () => {
-  return Sparky.src("src/app/index.html").dest(`${OUTPUT_DIR}/$name`)
-})
+  return Sparky.src("src/app/index.html").dest(`${OUTPUT_DIR}/$name`);
+});
 
 // the default task
-Sparky.task("default", ["copy-html"], () => {
+Sparky.task("default", ["copy-html"], async () => {
   // setup the producer with common settings
   const fuse = FuseBox.init({
-    homeDir: "src",
+    homeDir: SRC_DIR,
     output: `${OUTPUT_DIR}/$name.js`,
     target: "electron",
     log: isProduction,
     cache: !isProduction,
     sourceMaps: true,
     tsConfig: "tsconfig.json",
-  })
+  });
 
   // start the hot reload server
   if (!isProduction) {
-    fuse.dev({ port: DEV_PORT, httpServer: false })
+    fuse.dev({ port: DEV_PORT, httpServer: false });
   }
 
   // bundle the electron main code
@@ -42,24 +45,32 @@ Sparky.task("default", ["copy-html"], () => {
       ReplacePlugin({
         "process.env.HOMEPAGE": pjson.homepage ? `"${pjson.homepage}"` : "null",
       }),
-    )
+    );
 
   // and watch unless we're bundling for production
   if (!isProduction) {
-    mainBundle.watch()
+    mainBundle.watch();
   }
 
   // bundle the electron renderer code
   const rendererBundle = fuse
     .bundle("renderer")
     .instructions("> [app/index.tsx] +fuse-box-css")
-    .plugin(CSSPlugin())
-    .plugin(CopyPlugin({ useDefault: false, files: ASSETS, dest: "assets", resolve: "assets/" }))
+    .plugin(CSSPlugin());
+
+  const getAllFilesFromDir = async dir => {
+    const files = await fs.promises.readdir(dir);
+    files.map(file => {
+      return (await fs.promises.stat(dir + file)).isDirectory()
+        ? getAllFilesFromDir(path.join(dir, file, "/"))
+        : file;
+    });
+  };
 
   // and watch & hot reload unless we're bundling for production
   if (!isProduction) {
-    rendererBundle.watch()
-    rendererBundle.hmr()
+    rendererBundle.watch();
+    rendererBundle.hmr();
   }
 
   // when we are finished bundling...
@@ -69,9 +80,9 @@ Sparky.task("default", ["copy-html"], () => {
       spawn("node", [`${__dirname}/node_modules/electron/cli.js`, __dirname], {
         stdio: "inherit",
       }).on("exit", code => {
-        console.log(`electron process exited with code ${code}`)
-        process.exit(code)
-      })
+        console.log(`electron process exited with code ${code}`);
+        process.exit(code);
+      });
     }
-  })
-})
+  });
+});
